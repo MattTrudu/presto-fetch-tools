8d9aa2140ef7adc214f9c1036fbd42a0e4e5913e
Matt Trudu <trudumatteo@outlook.com>
2025-09-15 12:13:48 +0200

    bug fix

--- setup.py
+++ setup.py
@@ -4,17 +4,7 @@ setup(
     name="presto-fetch-tools",
     version="0.1.0",
     description="Convert PRESTO .singlepulse outputs into FETCH-compatible CSV files",
-    author="Matteo Trudu",
-    author_email="matteo@trudu.inaf",
-    url="https://github.com/MattTrudu/presto-fetch-tools",
-    scripts=["bin/make_presto_csv.py"], 
+    scripts=["bin/make_presto_csv.py"],  # <-- path to your script in the repo
     python_requires=">=3.7",
-    install_requires=[],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Topic :: Scientific/Engineering :: Astronomy",
-    ],
 )
 
