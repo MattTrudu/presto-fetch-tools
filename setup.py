a62f91c1e929bf858c5d876ed68ea45c1cb6b834
Matt Trudu <trudumatteo@outlook.com>
2025-09-15 12:15:33 +0200

    bug fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="presto-fetch-tools",
     version="0.1.0",
     description="Convert PRESTO .singlepulse outputs into FETCH-compatible CSV files",
-    scripts=["bin/make_presto_csv.py"],  # <-- path to your script in the repo
+    scripts=["make_presto_csv.py"],  # <-- path to your script in the repo
     python_requires=">=3.7",
 )
 
