7bd64d4d47fe9891044bc11f68873e057e8bb0ab
Matt Trudu <trudumatteo@outlook.com>
2025-09-25 14:52:36 +0200

    bug fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="presto-fetch-tools",
     version="0.1.1",
     description="Convert PRESTO .singlepulse outputs into FETCH-compatible CSV files",
-    scripts=["make_presto_csv.py","cands_overview.py"],   # installs a runnable 'make_presto_csv.py' into <env>/bin
-    py_modules=["make_presto_csv", "cands_overview"],   # also installs the module so you can do: python -m make_presto_csv
+    scripts=["make_presto_csv.py","cands_overview.py", "cands_sifter.py"],   # installs a runnable 'make_presto_csv.py' into <env>/bin
+    py_modules=["make_presto_csv", "cands_overview", "cands_sifter"],   # also installs the module so you can do: python -m make_presto_csv
     python_requires=">=3.7",
 )
